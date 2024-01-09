6a7f14405d12850623bf6b2c895880845e39f07b
tonserbin <117540888+tonserbin@users.noreply.github.com>
2024-01-09 04:21:19 +0300

    сделать слайдер

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,38 @@
 function initCarousel() {
-  // ваш код...
-}
+  let carousel = document.querySelector('.carousel');
+  let arrowRight = document.querySelector('.carousel__arrow_right');
+  let arrowLeft = document.querySelector('.carousel__arrow_left');
+  let carouselInner = document.querySelector('.carousel__inner');
+  let carouselList = document.querySelectorAll('.carousel__slide');
+  let showNum = 1;
+  let offset = carouselInner.offsetWidth;
+
+  arrowLeft.style.display = 'none';
+
+  carousel.addEventListener('click', (event) => {
+    if (event.target.closest('.carousel__arrow_right')) {
+      if (showNum < carouselList.length - 1) {
+        carouselInner.style.transform = `translateX(-${offset * showNum}px)`;
+        showNum++;
+        arrowLeft.style.display = '';
+      } else {
+        carouselInner.style.transform = `translateX(-${offset * showNum}px)`;
+        showNum++
+        arrowRight.style.display = 'none';
+        arrowLeft.style.display = '';
+      }
+    };
+
+    if (event.target.closest('.carousel__arrow_left')) {
+      if (showNum === 2) {
+        carouselInner.style.transform = `translateX(0px)`;
+        showNum--;
+        arrowLeft.style.display = 'none';
+      } else {
+        carouselInner.style.transform = `translateX(-${offset * (showNum - 2)}px)`;
+        showNum--;
+        arrowRight.style.display = '';
+      }
+    };
+  });
+};
